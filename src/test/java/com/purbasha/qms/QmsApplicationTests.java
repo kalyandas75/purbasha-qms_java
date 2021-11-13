2a2b884bb8cb03926f9c9bdefc95522d9f57e086
Kalyan <technakriti@gmail.com>
2021-11-12 22:14:32 -0600

    pom, gitignore

---
+++ src/test/java/com/purbasha/qms/QmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purbasha.qms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
